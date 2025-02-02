66eded65f9b03d182b807861710590c91faecf97
Yanujz <yanujz@live.it>
2025-02-02 22:40:01 +0100

    improve finder-test.sh

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -12,57 +12,71 @@ username=$(cat conf/username.txt)
 
 if [ $# -lt 3 ]
 then
-	echo "Using default value ${WRITESTR} for string to write"
-	if [ $# -lt 1 ]
-	then
-		echo "Using default value ${NUMFILES} for number of files to write"
-	else
-		NUMFILES=$1
-	fi	
+    echo "Using default value ${WRITESTR} for string to write"
+    if [ $# -lt 1 ]
+    then
+        echo "Using default value ${NUMFILES} for number of files to write"
+    else
+        NUMFILES=$1
+    fi  
 else
-	NUMFILES=$1
-	WRITESTR=$2
-	WRITEDIR=/tmp/aeld-data/$3
+    NUMFILES=$1
+    WRITESTR=$2
+    WRITEDIR=/tmp/aeld-data/$3
 fi
 
 MATCHSTR="The number of files are ${NUMFILES} and the number of matching lines are ${NUMFILES}"
 
 echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 
-rm -rf "${WRITEDIR}"
+# Check if WRITEDIR exists, remove it before creating it again
+if [ -d "$WRITEDIR" ]; then
+    rm -rf "$WRITEDIR"
+fi
 
-# create $WRITEDIR if not assignment1
+# create WRITEDIR if not exists (only if not assignment1)
 assignment=`cat ../conf/assignment.txt`
 
 if [ $assignment != 'assignment1' ]
 then
-	mkdir -p "$WRITEDIR"
+    mkdir -p "$WRITEDIR"
 
-	#The WRITEDIR is in quotes because if the directory path consists of spaces, then variable substitution will consider it as multiple argument.
-	#The quotes signify that the entire string in WRITEDIR is a single string.
-	#This issue can also be resolved by using double square brackets i.e [[ ]] instead of using quotes.
-	if [ -d "$WRITEDIR" ]
-	then
-		echo "$WRITEDIR created"
-	else
-		exit 1
-	fi
+    if [ -d "$WRITEDIR" ]
+    then
+        echo "$WRITEDIR created"
+    else
+        echo "Failed to create $WRITEDIR"
+        exit 1
+    fi
 fi
+
+# Clean and compile the writer application
 echo "Removing the old writer utility and compiling as a native application"
 make clean
-make CROSS_COMPILE=aarch64-none-linux-gnu-
 
-if [ $? -eq 0 ]; then
-mkdir -p assignments/assignment2
-file writer > assignments/assignment2/fileresult.txt
+# Compile with or without cross-compilation
+if [ -z ${CROSS_COMPILE+x} ]; then
+    make
+else
+    make CROSS_COMPILE=aarch64-none-linux-gnu-
 fi
 
+if [ $? -eq 0 ]; then
+    # Save the result of the 'file' command to fileresult.txt
+    mkdir -p assignments/assignment2
+    file writer > assignments/assignment2/fileresult.txt
+else
+    echo "Make failed. Exiting."
+    exit 1
+fi
 
+# Write the files
 for i in $( seq 1 $NUMFILES)
 do
-	./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+    ./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
+# Call the finder.sh script and capture the output
 OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
 
 # remove temporary directories
@@ -71,9 +85,9 @@ rm -rf /tmp/aeld-data
 set +e
 echo ${OUTPUTSTRING} | grep "${MATCHSTR}"
 if [ $? -eq 0 ]; then
-	echo "success"
-	exit 0
+    echo "success"
+    exit 0
 else
-	echo "failed: expected  ${MATCHSTR} in ${OUTPUTSTRING} but instead found"
-	exit 1
+    echo "failed: expected ${MATCHSTR} in ${OUTPUTSTRING} but instead found"
+    exit 1
 fi
